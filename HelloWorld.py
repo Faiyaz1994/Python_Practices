b2f04b987f25eeb922c57e90e8068cfe5cfe9cfd
Faiyaz1994 <faiyaz1994@gmail.com>
2023-08-24 22:44:06 +0600

    New file added

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,17 @@
 print("Hello World")
 print("Welcome to my profile")
 print("I hope I'll succeed")
+
+message= "Hello python World"
+message="This is new line"
+print(message)
+
+name= "ali noor faiyaz"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name="Ali Noor"
+last_name="Faiyaz"
+full_name=first_name+" "+last_name
+print("Hello "+full_name)
